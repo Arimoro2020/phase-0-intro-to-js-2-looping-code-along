69ba2b35581ef3be77bd16b78cba3210c022bc74
Adeyemi Arimoro <arimoro71@gmail.com>
2023-02-10 21:55:49 -0600

    Added solution to index.js

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Code your solutions in this file
+
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+    //debugger;
+}
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    //debugger;
+  }
+
+  return gifts;
+}
+
+//wrapGifts(gifts);
+
+
+function writeCards(nameArray, eventName) {
+  let allMessage = [];
+  for (let i = 0; i < nameArray.length; i++) {
+    let myMessage = [`Thank you, ${nameArray[i]}, for the wonderful ${eventName} gift!`];
+    allMessage = [...allMessage, ...myMessage];
+  }
+  return allMessage;
+}
+
+//writeCards(["Charlie", "Samip", "Ali"],"birthday");
+
+function countDown(number) {
+  let i = number;
+  while (i >= 0) {
+    console.log(i);
+    i--;
+  }
+}
+
